59b237a99c460e599a96e3056d6a5a2aeedf561b
Joshua T. Fisher <playmer@gmail.com>
2012-09-07 19:35:38 -0400

    Initial Commit

---
+++ Project Euler Q2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Project Euler Q2
+//
+//  Created by Joshua Fisher on 9/7/12.
+//  Copyright (c) 2012 Joshua Fisher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
